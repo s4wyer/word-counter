40182703e47a7789f409e136753a9acd9507e694
Sawyer <s4wyer@users.noreply.github.com>
2025-03-08 15:44:09 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# word-counter
+
+A simple svelte word counter
+
+## Developing
+
+```bash
+# should work fine with normal npm too
+pnpm install
+pnpm run dev -- --open
+```
+
+## Building
+
+To create a production version:
+
+```bash
+pnpm run build
+```
+
+You can preview the production build with `pnpm run preview`.
---
+++ src/routes/+page.svelte
@@ -0,0 +1,46 @@
+<script>
+	import { browser } from '$app/environment';
+	import { onMount } from 'svelte';
+	import { countWords } from 'alfaaz';
+
+	let text;
+
+	function wordCount(text) {
+		if (text == undefined) {
+			return '0';
+		} else {
+			return countWords(text);
+		}
+	}
+
+	function characterCount(text) {
+		if (text == undefined) {
+			return '0';
+		} else {
+			return text.length;
+		}
+	}
+
+	onMount(() => {
+		if (browser) {
+			const savedText = localStorage.getItem('text');
+			if (savedText !== null) {
+				text = savedText; // Ensure reactivity
+			}
+		}
+	});
+
+	$: if (browser && text !== undefined) {
+		localStorage.setItem('text', text);
+	}
+</script>
+
+<svelte:head>
+	<title>Word Counter</title>
+</svelte:head>
+
+<main>
+	<p class="word-count center">{wordCount(text)} words, {characterCount(text)} characters</p>
+	<!-- svelte-ignore a11y_autofocus -->
+	<textarea class="center" autofocus bind:value={text}></textarea>
+</main>
